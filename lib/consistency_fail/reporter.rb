a49db34fe18852c7b9d804b0cd1deb1fe03e5446
Colin Jones <trptcolin@gmail.com>
2011-05-23 02:44:54 -0500

    Add specs for Reporter

--- lib/consistency_fail/reporter.rb
+++ lib/consistency_fail/reporter.rb
@@ -2,40 +2,50 @@ module ConsistencyFail
   class Reporter
     TERMINAL_WIDTH = 80
 
-    def report_has_one_problems(indexes_by_model)
-      if indexes_by_model.empty?
-        divider
-        puts "Hooray! All has_one calls are correctly backed by a unique index."
-        divider
-      else
-        indexes_by_table_name = indexes_by_model.map do |model, indexes|
-          ["#{model.name}", indexes]
-        end.sort_by(&:first)
-        model_header = "Offending Model"
-        longest_model_length = indexes_by_table_name.map(&:first).
-                                                     sort_by(&:length).
-                                                     last.
-                                                     length
-        longest_model_length = [longest_model_length, model_header.length].max
+#    def report_has_one_problems(indexes_by_model)
+#      if indexes_by_model.empty?
+#        divider
+#        puts "Hooray! All has_one calls are correctly backed by a unique index."
+#        divider
+#      else
+#        indexes_by_table_name = indexes_by_model.map do |model, indexes|
+#          ["#{model.name}", indexes]
+#        end.sort_by(&:first)
+#        model_header = "Offending Model"
+#        longest_model_length = indexes_by_table_name.map(&:first).
+#                                                     sort_by(&:length).
+#                                                     last.
+#                                                     length
+#        longest_model_length = [longest_model_length, model_header.length].max
+#
+#        puts
+#        divider(longest_model_length * 2)
+#        puts "has_one calls not backed by unique indexes"
+#        divider(longest_model_length * 2)
+#        print model_header.ljust(longest_model_length + 2)
+#        puts "Desired Unique Index (order is irrelevant)"
+#        divider(longest_model_length * 2)
+#
+#        indexes_by_table_name.each do |table, indexes|
+#          indexes.each do |index|
+#            print table.ljust(longest_model_length + 2)
+#            print "#{index.table_name}: "
+#            puts "(#{index.columns.join(", ")})"
+#          end
+#        end
+#        divider(longest_model_length * 2)
+#      end
+#      puts
+#    end
 
-        puts
-        divider(longest_model_length * 2)
-        puts "has_one calls not backed by unique indexes"
-        divider(longest_model_length * 2)
-        print model_header.ljust(longest_model_length + 2)
-        puts "Desired Unique Index (order is irrelevant)"
-        divider(longest_model_length * 2)
-
-        indexes_by_table_name.each do |table, indexes|
-          indexes.each do |index|
-            print table.ljust(longest_model_length + 2)
-            print "#{index.table_name}: "
-            puts "(#{index.columns.join(", ")})"
-          end
-        end
-        divider(longest_model_length * 2)
-      end
+    def header(table_header_text, longest_model_length)
       puts
+      divider(longest_model_length * 2)
+      puts "validates_uniqueness calls not backed by unique indexes"
+      divider(longest_model_length * 2)
+      print table_header_text.ljust(longest_model_length + 2)
+      puts "Columns (multicolumn order is alphabetical)"
+      divider(longest_model_length * 2)
     end
 
     def report_validates_uniqueness_problems(indexes_by_model)
@@ -47,20 +57,14 @@ module ConsistencyFail
         indexes_by_table_name = indexes_by_model.map do |model, indexes|
           [model.table_name, indexes]
         end.sort_by(&:first)
-        table_header = "Table"
+        table_header_text = "Table"
         longest_model_length = indexes_by_table_name.map(&:first).
                                                      sort_by(&:length).
                                                      last.
                                                      length
-        longest_model_length = [longest_model_length, table_header.length].max
+        longest_model_length = [longest_model_length, table_header_text.length].max
 
-        puts
-        divider(longest_model_length * 2)
-        puts "validates_uniqueness calls not backed by unique indexes"
-        divider(longest_model_length * 2)
-        print table_header.ljust(longest_model_length + 2)
-        puts "Columns (multicolumn order is alphabetical)"
-        divider(longest_model_length * 2)
+        header(table_header_text, longest_model_length)
 
         indexes_by_table_name.each do |table, indexes|
           indexes.each do |index|
---
+++ spec/reporter_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'consistency_fail/reporter'
+require 'consistency_fail/index'
+
+describe ConsistencyFail::Reporter do
+  before(:each) do
+    @real_out = $stdout
+    @fake_out = StringIO.new
+    $stdout = @fake_out
+  end
+  after(:each) do
+    $stdout = @real_out
+  end
+
+  it "says everything's good" do
+    subject.report_validates_uniqueness_problems([])
+
+    @fake_out.string.should =~ /Hooray!/
+  end
+
+  it "shows a missing single-column index on a single model" do
+    missing_indexes = [ConsistencyFail::Index.new("users", ["email"])]
+
+    subject.report_validates_uniqueness_problems(fake_ar_model("User", :table_name => "users") => missing_indexes)
+
+    @fake_out.string.should =~ /users\s+\(email\)/
+  end
+
+  it "shows a missing multiple-column index on a single model" do
+    missing_indexes = [ConsistencyFail::Index.new("address", ["number", "street", "zip"])]
+
+    subject.report_validates_uniqueness_problems(fake_ar_model("Address", :table_name => "addresses") => missing_indexes)
+
+    @fake_out.string.should =~ /addresses\s+\(number, street, zip\)/
+  end
+
+  context "with problems on multiple models" do
+    before(:each) do
+      subject.report_validates_uniqueness_problems(
+        fake_ar_model("User", :table_name => "users") =>
+          [ConsistencyFail::Index.new("users", ["email"])],
+        fake_ar_model("Citizen", :table_name => "citizens") =>
+          [ConsistencyFail::Index.new("citizens", ["ssn"])]
+      )
+    end
+
+    it "shows all problems" do
+      @fake_out.string.should =~ /users\s+\(email\)/
+      @fake_out.string.should =~ /citizens\s+\(ssn\)/
+    end
+
+    it "orders the models alphabetically" do
+      @fake_out.string.should =~ /citizens\s+\(ssn\).*users\s+\(email\)/m
+    end
+  end
+end
