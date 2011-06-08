58724a5154f646da38ab29f0d943db3f7213961b
Colin Jones <trptcolin@gmail.com>
2011-06-08 08:53:11 -0500

    Provide a mega-fail to be used from an initializer

---
+++ lib/consistency_fail/enforcer.rb
@@ -0,0 +1,56 @@
+require 'consistency_fail'
+
+module ConsistencyFail
+  class Enforcer
+    def problems(models, introspector)
+      problems = models.map do |m|
+        [m, introspector.missing_indexes(m)]
+      end.reject do |m, indexes|
+        indexes.empty?
+      end
+    end
+
+    def self.enforce!
+      models = ConsistencyFail::Models.new($LOAD_PATH)
+      models.preload_all
+
+      introspectors = [ConsistencyFail::Introspectors::ValidatesUniquenessOf.new,
+                       ConsistencyFail::Introspectors::HasOne.new]
+
+      problem_models_exist = models.all.detect do |model|
+        introspectors.any? {|i| !i.missing_indexes(model).empty?}
+      end
+
+      if problem_models_exist
+        mega_fail!
+      end
+    end
+
+    def self.mega_fail!
+      ActiveRecord::Base.class_eval do
+        class << self
+          def panic
+            raise "You've got missing indexes! Run `consistency_fail` to find and fix them."
+          end
+
+          def find(*arguments)
+            panic
+          end
+
+          alias :first :find
+          alias :last :find
+          alias :count :find
+        end
+
+        def save
+          self.class.panic
+        end
+
+        def save!
+          self.class.panic
+        end
+      end
+    end
+
+  end
+end
