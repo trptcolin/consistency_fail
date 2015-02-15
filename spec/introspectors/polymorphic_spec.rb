c478efc79ba000f3cdf2dee65a2a9f47ce3cf3d8
Logan Leger <logan@loganleger.com>
2015-02-15 15:05:45 -0600

    Check for emptiness in tests versus explicit empty array

--- spec/introspectors/polymorphic_spec.rb
+++ spec/introspectors/polymorphic_spec.rb
@@ -14,7 +14,7 @@ describe ConsistencyFail::Introspectors::Polymorphic do
     end
 
     it "finds none when they're already in place" do
-      expect(subject.missing_indexes(CorrectPost)).to eq([])
+      expect(subject.missing_indexes(CorrectPost)).to be_empty
     end
   end
   
